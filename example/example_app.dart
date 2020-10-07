c94d3cf4a454a467133236ffdab7abcee15aa238
safalstha142 <computerstha12@gmail.com>
2020-10-07 10:06:57 +0545

    thrid commit

--- example/example_app.dart
+++ example/example_app.dart
@@ -1,21 +1,3 @@
-# picker
-
-A new Flutter package project.
-
-## Getting Started
-
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
-### Example
-
-```dart
-
 import 'package:cpicker/picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -88,5 +70,3 @@ class _HomeState extends State<Home> {
         )));
   }
 }
-
-
